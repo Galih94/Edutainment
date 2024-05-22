6eafc344cda47044ef95ecb44c40b46390326f2d
galih <samudragalih94@gmail.com>
2024-05-22 14:16:15 +0700

    wip initial project

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Galih Samudra on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
