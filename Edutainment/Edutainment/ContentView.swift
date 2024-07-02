c5bedefa34cb975597826b37672854da34f7b9c8
galih <samudragalih94@gmail.com>
2024-07-02 13:46:46 +0700

    feat(UI): create state of games

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -7,15 +7,71 @@
 
 import SwiftUI
 
+enum StateGame {
+    case setting, active, finish
+}
+
 struct ContentView: View {
+    @State private var currentState: StateGame = .setting
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        switch currentState {
+        case .setting:
+            ZStack {
+                Color.gray
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Setting Game")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .bold()
+                    Button("Start") {
+                        currentState = .active
+                    }
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                    .bold()
+                    
+                }
+                .padding()
+            }
+        case .active:
+            ZStack {
+                Color.green
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Active Game")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .bold()
+                    Button("Result") {
+                        currentState = .finish
+                    }
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                    .bold()
+                }
+                .padding()
+            }
+        case .finish:
+            ZStack {
+                Color.brown
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Result Game")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .bold()
+                    Button("Back to Setting") {
+                        currentState = .setting
+                    }
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                    .bold()
+                    
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
